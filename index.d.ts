d66d4fdb1ec07b698fa7b85c18d1b77706131006
Sergey Tominoff <ikenfin@gmail.com>
2021-03-02 14:00:02 +0400

    configure sentry types loading from sub-package

--- index.d.ts
+++ index.d.ts
@@ -3,83 +3,11 @@ import type { Plugin } from 'vite'
 /*
   Common types
 */
-export interface SentryCliOptions {
-  silent: boolean
-  org: string
-  project: string
-  authToken: string
-  url?: string
-  vcsRemote?: string
-}
-
-export interface SentryCliUploadSourceMapsOptions {
-  include: string | string[]
-  ignore?: string[]
-  ignoreFile?: string | null
-  rewrite?: boolean
-  sourceMapReference?: boolean
-  stripPrefix?: string[]
-  stripCommonPrefix?: boolean
-  validate?: boolean
-  urlPrefix?: string
-  urlSuffix?: string
-  ext?: string[]
-}
-
-export interface SentryCliNewDeployOptions {
-  env: string
-  started?: number
-  finished?: number
-  time?: number
-  name?: string
-  url?: string
-}
-
-export interface SentryCliCommitsOptions {
-  auto?: boolean
-  repo?: string
-  commit?: string
-  previousCommit?: string
-}
-
-/*
-  Typings for untyped sentry cli
-*/
-declare module '@sentry/cli' {
-  export interface SentryCliReleases {
-    ['new'](
-      release: string,
-      options?: { projects: string[] } | string[]
-    ): Promise<string>
-    setCommits(
-      release: string,
-      options: SentryCliCommitsOptions
-    ): Promise<string>
-    finalize(release: string): Promise<string>
-    proposeVersion(): Promise<string>
-    uploadSourceMaps(
-      release: string,
-      options: SentryCliUploadSourceMapsOptions
-    ): Promise<string>
-    newDeploy(
-      release: string,
-      options: SentryCliNewDeployOptions
-    ): Promise<string>
-  }
-
-  export interface SentryCliInstance {
-    releases: SentryCliReleases
-    options: SentryCliOptions
-  }
-
-  export default class SentryCli {
-    constructor(configPath?: string | null, options?: SentryCliOptions)
-    public releases: SentryCliReleases
-    public static getVersion(): string
-    public static getPath(): string
-    public static execute(): Promise<string>
-  }
-}
+import type {
+  SentryCliCommitsOptions,
+  SentryCliUploadSourceMapsOptions,
+  SentryCliNewDeployOptions
+} from '@sentry/cli'
 
 /*
     Plugin input options
