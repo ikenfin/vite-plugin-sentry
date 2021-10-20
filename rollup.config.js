18261aa2b8abcd89ec4c9881dca5660f597a5473
Sergey Tominoff <ikenfin@gmail.com>
2021-10-20 07:55:34 +0400

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 // rollup.config.js
 import pkg from './package.json'
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import babelrc from 'babelrc-rollup'
-import commonjs from 'rollup-plugin-commonjs'
-import { eslint } from 'rollup-plugin-eslint'
+import commonjs from '@rollup/plugin-commonjs'
+import eslint from '@rollup/plugin-eslint'
 import autoExternal from 'rollup-plugin-auto-external'
 import rollupTypescript from 'rollup-plugin-typescript2'
 
